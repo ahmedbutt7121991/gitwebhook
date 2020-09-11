1f186c82fdfe6ec7ad1bbff8fd27b4dccac24be3
Ahmed Butt <ahmed.butt91@gmail.com>
2020-09-11 11:31:12 +0500

    pub repo trigger

---
+++ pub_trigger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo
+"
+Things Need to Check in a public Repo....
+
+#########	PUSH EVENT	#########
+
+1. Direct push to trigger and master branch.
+2. Pull trigger branch from master branch.
+3. Merge both branches.
+"
