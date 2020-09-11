0cb173f9866b34ddfccc0d3fdb203595e3954a23
Ahmed Butt <ahmed.butt91@gmail.com>
2020-09-11 09:52:02 +0500

    added trigger script

--- trigger.sh
+++ trigger.sh
@@ -3,4 +3,7 @@
 echo "
 I am on Brach Trigger....
 Checking trigger branch
+If Push is only selected the trigger works only on Push request Directly
+Lets see what happens when
+Pull is also selected....
 "
